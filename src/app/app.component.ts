47022d6614ce7fe1151d5c9e68d883b5567d3de3
diegoviannaporfirio <porfiriodiego64@gmail.com>
2026-03-08 14:55:30 -0300

    feat: Implement core application layout by integrating header, main content, and modal components.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { TaskCommentsModalComponent } from './components/task-comments-modal/task-comments-modal.component';
+import { TaskFormModalComponent } from './components/task-form-modal/task-form-modal.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    HeaderComponent,
+    TaskCommentsModalComponent,
+    TaskFormModalComponent,
+    MainContentComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'go-task';
